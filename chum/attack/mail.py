6e503b44ed68485193f46aab25947ff9b6b23eb8
Ian Koide <44419333+ikoide@users.noreply.github.com>
2020-10-30 14:22:40 -0400

    Update mail.py

--- chum/attack/mail.py
+++ chum/attack/mail.py
@@ -52,5 +52,5 @@ class AttackMail():
     self.send_mail()
 
 if __name__ == '__main__':
-  mail = AttackMail('t', 'MCJCGRCMWTPAYGCXZXCZ@gmail.com', 't', 'smtp.gmail.com', 587, '7qNXnRt8', '/home/lan/Documents/aps-sys/Chum/targets/targets.csv')
+  mail = AttackMail('t', 'MCJCGRCMWTPAYGCXZXCZ@gmail.com', 't', 'smtp.gmail.com', 587, '7qNXnRt8', 'path_to_csv')
   mail.run()
